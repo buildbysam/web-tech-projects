4273436256ba5958538378d254c2d6cf4ea94536
Sijal Manandhar <sijal.m06@gmail.com>
2025-12-15 03:54:11 +0545

    moved class projects, nextjs setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-2xl text-blue-400">Hello, World!</h1>
+    </div>
+  );
+}
