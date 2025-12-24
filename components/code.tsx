a03f889c1858fb87196268c44a710ccc5be69cab
Sijal Manandhar <sijal.m06@gmail.com>
2025-12-24 19:56:53 +0545

    use children in Code component and fix SyntaxHighlighter TS errors

--- components/code.tsx
+++ components/code.tsx
@@ -2,6 +2,7 @@
 
 import { firacode } from "@/lib/fonts";
 import { useTheme } from "next-themes";
+import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark, atomOneLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
@@ -14,11 +15,25 @@ interface Props {
 export default function Code({ language, className = "", children }: Props) {
   const { resolvedTheme } = useTheme();
   const isDark = resolvedTheme === "dark";
+  const codeString = React.Children.toArray(children).join("");
+
   const customStyle = {
     margin: 0,
     paddingLeft: 0,
     paddingRight: 0,
     background: "transparent",
+  };
+
+  const lineNumberStyle = {
+    minWidth: "3.2em",
+    paddingRight: "0.7em",
+    paddingLeft: "0.7em",
+    marginRight: "1em",
+    color: isDark ? "#6b7280" : "#9ca3af",
+    textAlign: "right" as const,
+    background: isDark ? "#2125304d" : "#f3f4f64d",
+    userSelect: "none" as const,
+    borderRight: isDark ? "1px solid #2A2F3C" : "1px solid #E5E7EB",
   };
 
   return (
@@ -28,20 +43,10 @@ export default function Code({ language, className = "", children }: Props) {
       style={isDark ? atomOneDark : atomOneLight}
       PreTag="div"
       showLineNumbers
-      lineNumberStyle={{
-        minWidth: "3.2em",
-        paddingRight: "0.7em",
-        paddingLeft: "0.7em",
-        marginRight: "1em",
-        color: isDark ? "#6b7280" : "#9ca3af",
-        textAlign: "right",
-        background: isDark ? "#2125304d" : "#f3f4f64d",
-        userSelect: "none",
-        borderRight: isDark ? "1px solid #2A2F3C" : "1px solid #E5E7EB",
-      }}
+      lineNumberStyle={lineNumberStyle}
       className={`text-sm overflow-auto ${firacode.className} ${className}`}
     >
-      {children}
+      {codeString}
     </SyntaxHighlighter>
   );
 }
